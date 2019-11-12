0a4f778e14a28bfc4bd0197ea355764d01ec5be6
Dmitriy Proshutin <dmitry.proshutin@gmail.com>
2019-11-12 17:49:00 +0600

    Логика для контейнера PageLoader реализована. В componentDidMount запускается axios запрос к базе данных для получения заголовка и основного контента выбранной через пропсы страницы. Эти данные записываются в state и рендерятся в виде HTML элементов h3 и p. componentDidUpdate получает предыдущие пропсы и сравнивает их с новыми. Если пользователь кликнул другую страницу в навигационном меню, то происходит повторный запрос к базе данных для получения контента для новой страницы.

--- src/containers/PageLoader/PageLoader.js
+++ src/containers/PageLoader/PageLoader.js
@@ -1,15 +1,37 @@
 import React, {Component} from 'react';
+import axios from '../../axios-homework-65';
 
 class PageLoader extends Component {
+    state = {
+        data: {},
+        loading: true
+    };
+    _shouldRequest = (prevProps) => {
+        return (prevProps.match.params.page !== this.props.match.params.page);
+    };
+    componentDidUpdate(prevProps) {
+        if (this._shouldRequest(prevProps)) {
+            this._getContent();
+        }
+    }
+    componentDidMount() {
+        this._getContent();
+    }
+    _getContent = () => {
+        axios.get("/" + this.props.match.params.page + ".json")
+            .then(response => {
+                const data = response.data;
+                this.setState({loading: false, data});
+            }).catch(err => {
+            this.setState({loading: false});
+            throw new Error(err);
+        })
+    };
     render() {
-        const {match: {params}} = this.props;
-
-        const {page} = params;
         return (
             <div>
-                <p>
-                    {page}
-                </p>
+                <h3>{this.state.data.title}</h3>
+                <p>{this.state.data.content}</p>
             </div>
         );
     }
